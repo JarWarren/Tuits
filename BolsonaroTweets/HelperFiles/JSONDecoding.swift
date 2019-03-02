c675df806890eb4ff87d2c23af403ef861267624
Jared Warren <gentlemanjaruna@gmail.com>
2019-03-01 22:00:25 -0700

    adds sample tweet JSON for testing

---
+++ BolsonaroTweets/HelperFiles/JSONDecoding.swift
@@ -0,0 +1,22 @@
+//
+//  JSONDecoding.swift
+//  BolsonaroTweets
+//
+//  Created by Jared Warren on 3/1/19.
+//  Copyright © 2019 Warren. All rights reserved.
+//
+
+import Foundation
+
+struct Tuit: Decodable {
+    
+    let text: String
+    let created_at: String
+    let user: User
+}
+
+struct User: Decodable {
+    
+    let name: String
+    let screen_name: String
+}
