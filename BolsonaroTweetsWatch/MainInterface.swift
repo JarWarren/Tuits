1fc039b08e96b225f719d9174f0203b988c569cd
JarWarren <gentlemanjaruna@gmail.com>
2019-07-19 22:37:11 -0600

    designs watchkit interface

---
+++ BolsonaroTweetsWatch/MainInterface.swift
@@ -0,0 +1,13 @@
+//
+//  MainInterface.swift
+//  BolsonaroTweets
+//
+//  Created by Jared Warren on 7/19/19.
+//  Copyright © 2019 Warren. All rights reserved.
+//
+
+import WatchKit
+
+class MainInterface: WKInterfaceController {
+
+}
